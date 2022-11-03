c05b8efc6e12458edf5e9056dc4c176d734cedf2
Saksithon2544 <saksithon.mt@technictrang.ac.th>
2022-11-03 10:37:38 +0700

    setUp

--- js/script.js
+++ js/script.js
@@ -1,6 +1,32 @@
-// some scripts
+fetch("./js/products.json").then((products) => products.json()).then(data => {
+	data.map((item) => {
+		$("#productsArea").append(`
+		<div class="col-xl-3 col-lg-3 col-md-4 col-6" >
+				<div class="card card-product-grid">
+					<a href="${item.url}" class="img-wrap"> <img src="${item.images}"> </a>
+					<figcaption class="info-wrap">
+						<ul class="rating-stars mb-1">
+							<li style="width:80%" class="stars-active">
+								<img src="images/icons/stars-active.svg" alt="">
+							</li>
+							<li>
+								<img src="images/icons/starts-disable.svg" alt="">
+							</li>
+						</ul>
+						<div>
+							<a href="#" class="text-muted">${item.product_type}</a>
+							<a href="#" class="title">${item.model}</a>
+						</div>
+						<div class="price h5 mt-2">฿ ${item.price}</div>
+					</figcaption>
+				</div>
+			</div>
+		`)
+	})
+})
 
-// jquery ready start
+
+
 $(document).ready(function() {
 	// jQuery code
 
@@ -76,45 +102,4 @@ $(document).ready(function() {
     }); 
     
 }); 
-// jquery end
 
-let http = new XMLHttpRequest();
-
-http.open('get', 'products.json', true);
-
-http.send();
-
-http.onload = function(){
-
-    if(this.readyState == 4 && this == 200){
-        let products = JSON.parse(this.responseText);
-        let output = "";
-        
-        for(let item of products){
-            output += `
-                    <div class="col-xl-3 col-lg-3 col-md-4 col-6" >
-                        <div class="card card-product-grid">
-                            <a href="${item.url}" class="img-wrap"> <img src="${item.images}"> </a>
-                            <figcaption class="info-wrap">
-                                <ul class="rating-stars mb-1">
-                                    <li style="width:80%" class="stars-active">
-                                        <img src="images/icons/stars-active.svg" alt="">
-                                    </li>
-                                    <li>
-                                        <img src="images/icons/starts-disable.svg" alt="">
-                                    </li>
-                                </ul>
-                                <div>
-                                    <a href="#" class="text-muted">${item.product_type}</a>
-                                    <a href="#" class="title">${item.model}</a>
-                                </div>
-                                <div class="price h5 mt-2">฿ ${item.price}</div>
-                            </figcaption>
-                        </div>
-                    </div>
-            `;
-        }
-
-        document.querySelector(".products").innerHTML = output;
-    }
-}
