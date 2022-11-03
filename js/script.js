10cdab4af1ef809d35e86c5851e4b3dfb79d0a37
Saksithon2544 <saksithon.mt@technictrang.ac.th>
2022-11-03 16:27:08 +0700

    detup

--- js/script.js
+++ js/script.js
@@ -28,19 +28,10 @@ fetch("./js/products.json").then((products) => products.json()).then(data => {
 
 
 $(document).ready(function () {
-	// jQuery code
-
-	// var html_download = '<a href="http://bootstrap-ecommerce.com/templates.html" class="btn btn-dark rounded-pill" style="font-size:13px; z-index:100; position: fixed; bottom:10px; right:10px;">Download theme</a>';
-	//  $('body').prepend(html_download);
-
-
-	//////////////////////// Prevent closing from click inside dropdown
 	$(document).on('click', '.dropdown-menu', function (e) {
 		e.stopPropagation();
 	});
 
-
-	///////////////// fixed menu on scroll for desctop
 	if ($(window).width() < 768) {
 
 		$('.nav-home-aside .title-category').click(function (e) {
@@ -53,31 +44,23 @@ $(document).ready(function () {
 			$(this).next().slideToggle('fast');
 		});
 
-	} // end if
+	}
 
-
-	// custom checkbox inside card effect
 	$('.js-check :radio').change(function () {
 		var check_attr_name = $(this).attr('name');
 		if ($(this).is(':checked')) {
 			$('input[name=' + check_attr_name + ']').closest('.js-check').removeClass('active');
 			$(this).closest('.js-check').addClass('active');
-			// item.find('.radio').find('span').text('Add');
-
 		} else {
 			item.removeClass('active');
-			// item.find('.radio').find('span').text('Unselect');
 		}
 
 	});
 
-	//////////////////////// Bootstrap tooltip
-	if ($('[data-toggle="tooltip"]').length > 0) {  // check if element exists
+	if ($('[data-toggle="tooltip"]').length > 0) {  
 		$('[data-toggle="tooltip"]').tooltip()
-	} // end if
+	} 
 
-
-	// offcanvas menu
 	$("[data-trigger]").on("click", function (e) {
 		e.preventDefault();
 		e.stopPropagation();
@@ -87,14 +70,13 @@ $(document).ready(function () {
 		$(".screen-overlay").toggleClass("show");
 	});
 
-	// Close menu when pressing ESC
 	$(document).on('keydown', function (event) {
 		if (event.keyCode === 27) {
 			$(".mobile-offcanvas").removeClass("show");
 			$("body").removeClass("overlay-active");
 		}
 	});
-	// Close menu by clicking
+
 	$(".btn-close, .screen-overlay").click(function (e) {
 		$(".screen-overlay").removeClass("show");
 		$(".mobile-offcanvas").removeClass("show");
